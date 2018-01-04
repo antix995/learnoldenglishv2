0a44d053c7f083a218cc65ae15ba9490d8e78b45
Nick <nick.id.matthews@gmail.com>
2018-01-04 10:30:53 +0000

    Deleted quiz.xml as should be unnecessary with activity_quiz Modified much of MainActivity.java by adding Quiz.Java lines and then deleting that Class as should also be unnecessary.

--- app/src/main/java/com/antixuni/learnoldenglishv2/MainActivity.java
+++ app/src/main/java/com/antixuni/learnoldenglishv2/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener,
         CompoundButton.OnCheckedChangeListener {
@@ -21,6 +23,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ShadowTransformer mFragmentCardShadowTransformer;
 
     private boolean mShowingFragments = false;
+
+    private QuestionLibrary mQuestionLibrary = new QuestionLibrary();
+    private TextView mScoreView;
+    private TextView mQuestionView;
+    private Button mButtonChoice1;
+    private Button mButtonChoice2;
+    private Button mButtonChoice3;
+
+    private String mAnswer;
+    private int mScore = 0;
+    private int mQuestionNumber = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +59,80 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mViewPager.setPageTransformer(false, mCardShadowTransformer);
         mViewPager.setOffscreenPageLimit(3);
 
+        mScoreView = (TextView)findViewById(R.id.score);
+        mQuestionView = (TextView)findViewById(R.id.question);
+        mButtonChoice1 = (Button)findViewById(R.id.choice1);
+        mButtonChoice2 = (Button)findViewById(R.id.choice2);
+        mButtonChoice3 = (Button)findViewById(R.id.choice3);
+
+        updateQuestion();
+
+        //Start of Button Listener for Button1
+        mButtonChoice1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //My logic for Button goes in here
+
+                if (mButtonChoice1.getText() == mAnswer){
+                    mScore = mScore + 1;
+                    updateScore(mScore);
+                    updateQuestion();
+                    //This line of code is optiona
+                    Toast.makeText(MainActivity.this, "correct", Toast.LENGTH_SHORT).show();
+
+                }else {
+                    Toast.makeText(MainActivity.this, "wrong", Toast.LENGTH_SHORT).show();
+                    updateQuestion();
+                }
+            }
+        });
+
+        //End of Button Listener for Button1
+
+        //Start of Button Listener for Button2
+        mButtonChoice2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //My logic for Button goes in here
+
+                if (mButtonChoice2.getText() == mAnswer){
+                    mScore = mScore + 1;
+                    updateScore(mScore);
+                    updateQuestion();
+                    //This line of code is optiona
+                    Toast.makeText(MainActivity.this, "correct", Toast.LENGTH_SHORT).show();
+
+                }else {
+                    Toast.makeText(MainActivity.this, "wrong", Toast.LENGTH_SHORT).show();
+                    updateQuestion();
+                }
+            }
+        });
+
+        //End of Button Listener for Button2
+
+
+        //Start of Button Listener for Button3
+        mButtonChoice3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //My logic for Button goes in here
+
+                if (mButtonChoice3.getText() == mAnswer){
+                    mScore = mScore + 1;
+                    updateScore(mScore);
+                    updateQuestion();
+                    //This line of code is optiona
+                    Toast.makeText(MainActivity.this, "correct", Toast.LENGTH_SHORT).show();
+
+                }else {
+                    Toast.makeText(MainActivity.this, "wrong", Toast.LENGTH_SHORT).show();
+                    updateQuestion();
+                }
+            }
+        });
+
+        //End of Button Listener for Button3
 
     }
 
@@ -73,4 +160,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mCardShadowTransformer.enableScaling(b);
         mFragmentCardShadowTransformer.enableScaling(b);
     }
+
+    private void updateQuestion(){
+        mQuestionView.setText(mQuestionLibrary.getQuestion(mQuestionNumber));
+        mButtonChoice1.setText(mQuestionLibrary.getChoice1(mQuestionNumber));
+        mButtonChoice2.setText(mQuestionLibrary.getChoice2(mQuestionNumber));
+        mButtonChoice3.setText(mQuestionLibrary.getChoice3(mQuestionNumber));
+
+        mAnswer = mQuestionLibrary.getCorrectAnswer(mQuestionNumber);
+        mQuestionNumber++;
+    }
+
+    private void updateScore(int point) {
+        mScoreView.setText("" + mScore);
+    }
+
 }
