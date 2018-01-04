f27cbd0aa7ce6365b4acb41413897defcde488fc
Nick <nick.id.matthews@gmail.com>
2018-01-04 15:37:48 +0000

    Commit for uploading project to GitHub

--- app/src/main/java/com/antixuni/learnoldenglishv2/MainActivity.java
+++ app/src/main/java/com/antixuni/learnoldenglishv2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.antixuni.learnoldenglishv2;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,12 +9,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener,
         CompoundButton.OnCheckedChangeListener {
 
+    Button mButtonQuiz;
     private Button mButton;
     private ViewPager mViewPager;
 
@@ -23,17 +23,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ShadowTransformer mFragmentCardShadowTransformer;
 
     private boolean mShowingFragments = false;
-
-    private QuestionLibrary mQuestionLibrary = new QuestionLibrary();
-    private TextView mScoreView;
-    private TextView mQuestionView;
-    private Button mButtonChoice1;
-    private Button mButtonChoice2;
-    private Button mButtonChoice3;
-
-    private String mAnswer;
-    private int mScore = 0;
-    private int mQuestionNumber = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,82 +48,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mViewPager.setPageTransformer(false, mCardShadowTransformer);
         mViewPager.setOffscreenPageLimit(3);
 
-        mScoreView = (TextView)findViewById(R.id.score);
-        mQuestionView = (TextView)findViewById(R.id.question);
-        mButtonChoice1 = (Button)findViewById(R.id.choice1);
-        mButtonChoice2 = (Button)findViewById(R.id.choice2);
-        mButtonChoice3 = (Button)findViewById(R.id.choice3);
-
-        updateQuestion();
-
-        //Start of Button Listener for Button1
-        mButtonChoice1.setOnClickListener(new View.OnClickListener(){
+        mButtonQuiz = (Button) findViewById(R.id.buttonQuiz);
+        mButtonQuiz.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                //My logic for Button goes in here
-
-                if (mButtonChoice1.getText() == mAnswer){
-                    mScore = mScore + 1;
-                    updateScore(mScore);
-                    updateQuestion();
-                    //This line of code is optiona
-                    Toast.makeText(MainActivity.this, "correct", Toast.LENGTH_SHORT).show();
-
-                }else {
-                    Toast.makeText(MainActivity.this, "wrong", Toast.LENGTH_SHORT).show();
-                    updateQuestion();
-                }
+                startActivity(new Intent(MainActivity.this, QuizActivity.class));
             }
         });
-
-        //End of Button Listener for Button1
-
-        //Start of Button Listener for Button2
-        mButtonChoice2.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                //My logic for Button goes in here
-
-                if (mButtonChoice2.getText() == mAnswer){
-                    mScore = mScore + 1;
-                    updateScore(mScore);
-                    updateQuestion();
-                    //This line of code is optiona
-                    Toast.makeText(MainActivity.this, "correct", Toast.LENGTH_SHORT).show();
-
-                }else {
-                    Toast.makeText(MainActivity.this, "wrong", Toast.LENGTH_SHORT).show();
-                    updateQuestion();
-                }
-            }
-        });
-
-        //End of Button Listener for Button2
-
-
-        //Start of Button Listener for Button3
-        mButtonChoice3.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                //My logic for Button goes in here
-
-                if (mButtonChoice3.getText() == mAnswer){
-                    mScore = mScore + 1;
-                    updateScore(mScore);
-                    updateQuestion();
-                    //This line of code is optiona
-                    Toast.makeText(MainActivity.this, "correct", Toast.LENGTH_SHORT).show();
-
-                }else {
-                    Toast.makeText(MainActivity.this, "wrong", Toast.LENGTH_SHORT).show();
-                    updateQuestion();
-                }
-            }
-        });
-
-        //End of Button Listener for Button3
 
     }
+
+    //public void openQuizActivity(){
+        //Intent intent = new Intent(this, QuizActivity.class);
+        //startActivity(intent);
+    //}
 
     @Override
     public void onClick(View view) {
@@ -159,20 +86,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
         mCardShadowTransformer.enableScaling(b);
         mFragmentCardShadowTransformer.enableScaling(b);
-    }
-
-    private void updateQuestion(){
-        mQuestionView.setText(mQuestionLibrary.getQuestion(mQuestionNumber));
-        mButtonChoice1.setText(mQuestionLibrary.getChoice1(mQuestionNumber));
-        mButtonChoice2.setText(mQuestionLibrary.getChoice2(mQuestionNumber));
-        mButtonChoice3.setText(mQuestionLibrary.getChoice3(mQuestionNumber));
-
-        mAnswer = mQuestionLibrary.getCorrectAnswer(mQuestionNumber);
-        mQuestionNumber++;
-    }
-
-    private void updateScore(int point) {
-        mScoreView.setText("" + mScore);
     }
 
 }
